4c498ee7d003944c6fabcd8503ce9b28bf300c5d
Flávia <Flvia@TGH-FL>
2017-07-11 21:47:41 +0100

    Test completes

--- phptravels.test/src/test/java/phptravels/test/TestSearchLocation.java
+++ phptravels.test/src/test/java/phptravels/test/TestSearchLocation.java
@@ -1,6 +1,7 @@
 package phptravels.test;
 
 
+import static org.junit.Assert.*;
 import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -16,7 +17,7 @@ public class TestSearchLocation {
 	
 
 	@Test
-	public void searchLocation(){
+	public void searchLocation() throws Exception{
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\EstudoFlavia\\chromedriver.exe");
 		int secondsToWait = 5;
@@ -45,60 +46,101 @@ public class TestSearchLocation {
 		// Check in date ======================================================
 		WebElement elementCheckIn = webdriver.findElement(By.name("checkin"));
 		elementCheckIn.click();
-				
+		
 		// Locate and wait to calendar be visible==============================
-		String xpathCalendarCheckIn = "/html/body/div[13]";
-		WebElement elementCalendarCheckIn = webdriver.findElement(By.xpath(xpathCalendarCheckIn));
-		explicitWait.until(ExpectedConditions.visibilityOf(elementCalendarCheckIn));
+		String xpathCalendarCheckIn13 = "/html/body/div[13]";
+		WebElement elementCalendarCheckIn13 = webdriver.findElement(By.xpath(xpathCalendarCheckIn13));
+		String xpathCalendarCheckIn14 = "/html/body/div[14]";
+		WebElement elementCalendarCheckIn14 = webdriver.findElement(By.xpath(xpathCalendarCheckIn14));
+		
+		explicitWait.until(
+				ExpectedConditions.or(
+						ExpectedConditions.visibilityOf(elementCalendarCheckIn13),
+						ExpectedConditions.visibilityOf(elementCalendarCheckIn14)
+				)
+		);
+		
+		WebElement locatedCheckIn = null;
+		if (elementCalendarCheckIn13.isDisplayed()) {
+			System.out.println("Carregou o 13");
+			locatedCheckIn = elementCalendarCheckIn13;
+		}
+		
+		if (elementCalendarCheckIn14.isDisplayed()) {
+			System.out.println("Carregou o 14");
+			locatedCheckIn = elementCalendarCheckIn14;
+		}
 		
 		String xpathNextMonthCheckIn = "div[1]/table/thead/tr[1]/th[3]";
-		WebElement elementNextMonthCheckIn = elementCalendarCheckIn.findElement(By.xpath(xpathNextMonthCheckIn));
+		WebElement elementNextMonthCheckIn = locatedCheckIn.findElement(By.xpath(xpathNextMonthCheckIn));
 		elementNextMonthCheckIn.click();
 				
 		String xpathChooseCheckIn = "div[1]/table/tbody/tr[4]/td[1]";
-		WebElement elementChooseCheckIn = elementCalendarCheckIn.findElement(By.xpath(xpathChooseCheckIn));
+		WebElement elementChooseCheckIn = locatedCheckIn.findElement(By.xpath(xpathChooseCheckIn));
 		elementChooseCheckIn.click();
 		
-		// Check out date ======================================================
-		WebElement elementCheckOut = webdriver.findElement(By.name("checkout"));
-		elementCheckOut.click();
-		
 		// Locate and wait to calendar be visible==============================
-		String xpathCalendarCheckOut = "/html/body/div[15]/div[1]";
-		WebElement elementCalendarCheckOut = webdriver.findElement(By.xpath(xpathCalendarCheckOut));
-		explicitWait.until(ExpectedConditions.visibilityOf(elementCalendarCheckOut));
+		String xpathCalendarCheckOut14 = "/html/body/div[14]";
+		WebElement elementCalendarCheckOut14 = webdriver.findElement(By.xpath(xpathCalendarCheckOut14));
+		String xpathCalendarCheckOut15 = "/html/body/div[15]";
+		WebElement elementCalendarCheckOut15 = webdriver.findElement(By.xpath(xpathCalendarCheckOut15));
+				
+		explicitWait.until(
+				ExpectedConditions.or(
+						ExpectedConditions.visibilityOf(elementCalendarCheckOut14),
+						ExpectedConditions.visibilityOf(elementCalendarCheckOut15)
+				)
+		);
+				
+		WebElement locatedCheckOut = null;
+			if (elementCalendarCheckOut14.isDisplayed()) {
+					System.out.println("Carregou o 14");
+					locatedCheckOut = elementCalendarCheckOut14;
+				}
+				
+			if (elementCalendarCheckOut15.isDisplayed()) {
+					System.out.println("Carregou o 15");
+					locatedCheckOut = elementCalendarCheckOut15;
+				}
+				
+				
 		
-		String xpathNextMonthCheckOut = "table/thead/tr[1]/th[3]";
-		WebElement elementNextMonthCheckOut = elementCalendarCheckOut.findElement(By.xpath(xpathNextMonthCheckOut));
+		String xpathNextMonthCheckOut = "/html/body/div[14]/div[1]/table/thead/tr[1]/th[3]";
+		WebElement elementNextMonthCheckOut = webdriver.findElement(By.xpath(xpathNextMonthCheckOut));
 		elementNextMonthCheckOut.click();
-		
-		String xpathChooseCheckOut = "table/tbody/tr[3]/td[7]";
-		WebElement elementChooseCheckOut = elementCalendarCheckOut.findElement(By.xpath(xpathChooseCheckOut));
+				
+				
+		String xpathChooseCheckOut = "/html/body/div[14]/div[1]/table/tbody/tr[5]/td[1]";
+		WebElement elementChooseCheckOut = webdriver.findElement(By.xpath(xpathChooseCheckOut));
 		elementChooseCheckOut.click();
-		
+				
 		// Adults===============================================================		
 		WebElement elementNumberAdults = webdriver.findElement(By.name("adults"));
 		elementNumberAdults.click();
-		
-		String xpathChooseNumberAdults = "";
+				
+		String xpathChooseNumberAdults = "//*[@id=\"adults\"]/option[1]";
 		WebElement elementChooseNumberAdults = webdriver.findElement(By.xpath(xpathChooseNumberAdults));
 		elementChooseNumberAdults.click();
-				
+						
 		WebElement elementNumberChild = webdriver.findElement(By.name("child"));
 		elementNumberChild.click();
-		
-		String xpathChooseNumberChild = "";
+				
+		String xpathChooseNumberChild = "//*[@id=\"child\"]/option[2]";
 		WebElement elementChooseNumberChild = webdriver.findElement(By.xpath(xpathChooseNumberChild));
 		elementChooseNumberChild.click();
-		
+				
 		String xpathButtonSearch = "//*[@id=\"HOTELS\"]/div/form/div[6]/div/button";
 		WebElement elementButtonSearch = webdriver.findElement(By.xpath(xpathButtonSearch));
 		elementButtonSearch.click();
-		
-		
-		
-		
-		
+				
+		String xpathNoResults = "/html/body/div[5]/div[3]/div/h1";
+		WebElement elementNoResults = webdriver.findElement(By.xpath(xpathNoResults));
+		assertEquals("No Results!!", elementNoResults.getText());
+				
+				
+				
+				
+				
 		
 		
 		
